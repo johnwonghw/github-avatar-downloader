8d152adebaab908b069ef167ec973212fe74faa8
John <john.wong.h@gmail.com>
2017-07-04 03:08:16 +0000

    added error functions if arguments are missing

--- download_avatars.js
+++ download_avatars.js
@@ -3,8 +3,6 @@ var fs = require('fs')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-var GITHUB_USER = "johnwonghw";
-var GITHUB_TOKEN = "90713039509b8029380ab7204e6c0d80566a59e9";
 
 function options (path) {
   return {
@@ -23,42 +21,41 @@ function getRepoContributors(repoOwner, repoName, cb) {
   request (options(path), function (error, response, body) {
     try {
       cb(JSON.parse(body));
-
     } catch (err) {
       console.log ("Errors: ", err)
     }
-
 })};
    // var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
    // console.log (requestURL)
 
-
-getRepoContributors (process.argv[2], process.argv[3], function (data) {
+var repoName = process.argv[2]
+var repoUser = process.argv[3]
+getRepoContributors (repoName, repoUser, function (data) {
+  if (repoName === undefined || repoUser === undefined) {
+    console.log ('Please enter both a Repository Name and the Repository Owner\'s Username')
+  } else {
   data.forEach(function (contributor) {
   var path = `./avatars/${contributor.login}.jpg`;
   downloadImageByURL(contributor.avatar_url, path);
-  });
+    })
+  };
 });
 
 
 
 function downloadImageByURL(url, filePath) {
-request.get(url)               // Note 1
-       .on('error', function (err) {                                   // Note 2
+request.get(url)
+       .on('error', function (err) {
          throw err;
        })
        .on ('request', function (response) {
          console.log('Downloading image...')
        })
-       .on('response', function (response) {                           // Note 3
+       .on('response', function (response) {
          console.log(' Response Status Message: ', response.statusMessage, '\n', 'Response Type: ', response.headers['content-type'])
        })
        .pipe(fs.createWriteStream(filePath))
        .on('finish', function() {
          console.log('Response stream complete.');
-    });            // Note 4
+    });
 }
-
-
-
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
