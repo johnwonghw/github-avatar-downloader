941af65cf50eec6f0e96a9ec2ea48bfd8cfa5247
John <john.wong.h@gmail.com>
2017-07-04 00:14:52 +0000

    added code to request URL

--- download_avatars.js
+++ download_avatars.js
@@ -2,16 +2,20 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = "johnwonghw";
+var GITHUB_TOKEN = "90713039509b8029380ab7204e6c0d80566a59e9";
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
 
 
-  getRepoContributors("jquery", "jquery", function(err, result) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+console.log (requestURL)
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
     console.log("Errors:", err);
     console.log("Result:", result);
   });
-
-
-
-}
